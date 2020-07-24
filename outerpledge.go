f46a10894594c3641f41572585176d5a8d491034
maxmalong <68720670+maxmalong@users.noreply.github.com>
2020-07-24 16:53:43 +0800

    Add files via upload

--- outerpledge.go
+++ outerpledge.go
@@ -10,31 +10,31 @@ func main() {
 	var interval,count,batch,total int
 	fmt.Printf("Please enter your config 'interval count batch total' like '10 5 60 200': ")
 	fmt.Scanln(&interval, &count,&batch,&total)
-	fmt.Println("interval=d%,count=d%,batch=d%,total=d%",interval,count,batch,total)
+	fmt.Println(fmt.Sprintf("interval=%d,count=%d,batch=%d,total=%d",interval,count,batch,total))
 
 	schedle(interval,count,batch,total,func()(error){
 		cmd := exec.Command("lotus-miner sectors pledge")
 		buf, err :=cmd.Output()
-		fmt.Println("任务执行结果返回：s%",buf)
+		fmt.Printf(fmt.Sprintf("任务执行结果返回：%s",buf))
 		return err
 	})
 
 }
-func schedle(interval,count,batch,total int,cc tefunc) error{
+func schedle(interval,count,batch,total int,cc tefunc) {
 	totalCount:=1
 	for {
 		for j := 0; j < count; j++ {
 			var err = cc()
 			if err!=nil{
-				fmt.Println("任务执行出错:s%",err)
+				fmt.Println("任务执行出错:",err)
 			}
-			fmt.Println("任务执行第d%次",totalCount)
+			fmt.Println(fmt.Sprintf("任务执行第%d次",totalCount))
 			totalCount++
-			time.Sleep(time.Second * 60* time.Duration(interval))
+			if (totalCount-total > 0){
+				return
+			}
+			time.Sleep(time.Second * 60 * time.Duration(interval))
 		}
-		if (totalCount-total > 0){
-			break
-		}
-		time.Sleep(time.Second * 60 *time.Duration(batch))
+		time.Sleep(time.Second * 60 * time.Duration(batch))
 	}
 }
